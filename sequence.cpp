c8b0a7f914e1aaaea3f3958bfeb5ddeeb5c14e4f
alea-atkinson <147665827+alea-atkinson@users.noreply.github.com>
2026-03-31 12:10:51 -0400

    make basic files and declare functions

---
+++ sequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "sequence.h"
+
+int main(){
+    return 0;
+}
+
+void genInput(int K){
+
+}
+
+int compute(){
+
+}
+
+void genOutput(){
+
+}
---
+++ sequence.h
@@ -0,0 +1,3 @@
+void genInput(int K);
+int compute();
+void genOutput();
