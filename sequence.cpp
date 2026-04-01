8a2df7d8a1e50266d40cdcba4616ff307e2021fc
kristenmoy <147666056+kristenmoy@users.noreply.github.com>
2026-04-01 18:01:25 -0400

    Get Max Sequence Value

--- sequence.cpp
+++ sequence.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
+#include <map>
 #include "sequence.h"
 
 int main(){
@@ -12,6 +14,40 @@ void genInput(int K){
 
 int compute(){
 
+}
+
+int getMaxSequenceValue(std::string A, std::string B, std::map<char, int> alphabet)
+{
+    int maxValue = 0;
+
+    // Initialize the solution vector M to 0
+    std::vector<std::vector<int>> M;
+    for(int i = 0; i < A.length(); i++)
+    {
+        for(int j = 0; j < B.length(); j++)
+        {
+            M[i][j] = 0;
+        }
+    }
+
+    // Calculate the values 
+    for(int i = 0; i < A.length(); i++)
+    {
+        int value = 0;
+
+        for(int j = 0; j < B.length(); j++)
+        {
+            if(i == j)
+            {
+                int value = alphabet[j];
+            }
+
+            M[i][j] = std::max(value + M[i - 1][j - 1], M[i][j-1]);
+            maxValue = M[i][j];
+        }
+    }
+    
+    return maxValue;
 }
 
 void genOutput(){
